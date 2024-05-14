4687b56835ed9ea07fe128493c300593d20f24e0
MohammedGhouseP <ghousebashamail@gmail.com>
2024-05-14 22:17:30 +0530

    population fetching API

--- Practices/fetch/country/country.js
+++ Practices/fetch/country/country.js
@@ -1,33 +1,43 @@
-let container = document.querySelector("#container")
+let container = document.querySelector("#container");
+let sel = document.getElementById("select") 
+let btn = document.getElementById("btn");
+btn.addEventListener("click",()=>{
+    
+    country(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries?sort=population&order=${sel.value}`)
+})
 
-function country(URL){
-    fetch(URL)
+function country(url){
+    fetch(url)
     .then(function(res){
         return res.json();
     })
     .then(function(res){
-        console.log(res.data);
+        // console.log(res.data);
         renderfunc(res.data);
-    });
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
 };
 
 
-function renderfunc(res){
-    console.log(res);
-    res.map(function(ele,i) {
+function renderfunc(url){
+    console.log(url);
+    url.map(function(ele,i) {
+        // container.innerHTML="";
 
         let box = document.createElement("div");
 
         let h3 = document.createElement("h4");
-        h3.innerHTML = ele.country;
+        h3.innerHTML = `Name: ${ele.country}`;
 
-        let h2 = document.createElement("h4");
-        h2.innerHTML = ele.rank;
+        // let h2 = document.createElement("h4");
+        // h2.innerHTML = ele.rank;
     
         let h5 = document.createElement("h5");
-        email.innerHTML = ele.population;
+        h5.innerHTML = `population: ${ele.population}`;
 
-        box.append(h3,h2,h5);
+        box.append(h3,h5);
         container.append(box);
     });
      
@@ -35,7 +45,6 @@ function renderfunc(res){
 
 
 
-country(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries`)
 
 
 
